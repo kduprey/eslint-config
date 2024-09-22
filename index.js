50195dc2548cd892e30c1e52b39ac506d2d1001d
Kenton Duprey <kenton@hausofweb.com>
2024-09-22 18:04:59 -0400

    fix(lib): Update package.json and index.js exports
    This commit refactors the package.json file and updates the exports in
    the index.js file. The package.json file now specifies the correct path for the
    index.js file in the exports section. Additionally, the index.js file has been
    modified to export the library, next, prettier, react, and server modules.
    
    Refactor the package.json and index.js exports
    
    Signed-off-by: Kenton Duprey <kenton@hausofweb.com>

---
+++ index.js
@@ -0,0 +1,13 @@
+import library from "./library";
+import next from "./next";
+import prettier from "./prettier";
+import react from "./react";
+import server from "./server";
+
+module.exports = {
+  library,
+  next,
+  prettier,
+  react,
+  server,
+};
